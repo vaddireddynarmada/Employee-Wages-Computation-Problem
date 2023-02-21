b2d48c10697ad523413a1c8ebfa013e5be49ee45
unknown <narmada.vaddireddy97@gmail.com>
2023-02-21 21:24:21 +0530

    Added: Wages till a condition of total working hours or days is reached for a month by Assume 100 hours and 20 days

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,27 +1,31 @@
-public class Uc5 {
+public class Uc6 {
     public static void main(String[] args) {
         int wagePerHour = 20;
         int workingDay = 20;
-        int attendance = (int) (Math.random() * 100) % 3;
-        int workingHour = 0;
+        int totalWage = 0;
 
-        switch (attendance) {
-            case 0:
-                workingHour = 8;
-                System.out.println("employee worked for full time");
-                break;
+        for (int i=1;i<=workingDay;i++) {
+            int attendance = (int) (Math.random() * 100) % 3;
+            int workingHour = 0;
+            switch (attendance) {
+                case 0:
+                    workingHour = 8;
+                    System.out.println("employee worked for full time");
+                    break;
 
-            case 1:
-                workingHour = 4;
-                System.out.println("employee worked for part time");
-                break;
+                case 1:
+                    workingHour = 4;
+                    System.out.println("employee worked for part time");
+                    break;
 
-            default:
-                System.out.println("Absent");
+                default:
+                    System.out.println("employee is Absent");
 
+            }
+            int wage = wagePerHour * workingHour;
+            System.out.println("Employee daily wage: " + wage + " Rupees.");
+            totalWage = wage + totalWage;
         }
-        int wage = (wagePerHour * workingHour) * workingDay;
-        System.out.println("Employee Monthly wage: " + wage + " Rupees.");
-
+        System.out.println("Employee monthly wage: " + totalWage + " Rupees.");
     }
 }
